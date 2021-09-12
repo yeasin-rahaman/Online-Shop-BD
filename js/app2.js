a6987e2f4d7deddef6c17df01c6ab0445cb50ab3
Dev70 <Shanto9585>
2021-09-13 04:41:38 +0600

    update

--- js/app2.js
+++ js/app2.js
@@ -10,18 +10,27 @@ const showProducts = (products) => {
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
-      </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <p>Rating by People:${product.rating.rate} <i class="fa fa-star" aria-hidden="true"></i></p>
-      <p>How many people rated : ${product.rating.count}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
-      `;
+    div.innerHTML = `  <div class="col">
+    <div class="card h-100">
+        <img class="product-image anything" style="
+        width: 160px;
+        height: 150;
+    " src=${image}></img>
+        <div class="card-body">
+            <h3>${product.title}</h3>
+            <p>Category: ${product.category}</p>
+            <p>Rating by People:${product.rating.rate} <i class="fa fa-star" aria-hidden="true"></i></p>
+            <p>How many people rated : ${product.rating.count}</p>
+            <h2>Price: $ ${product.price}</h2>
+        </div>
+        <div class="card-footer">
+            <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn"
+                class="buy-now btn btn-success">add to cart</button>
+            <button id="details-btn" class="btn btn-danger">Details</button>
+        </div>
+    </div>
+</div>
+`;
     document.getElementById("all-products").appendChild(div);
   }
 };
